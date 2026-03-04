a8225fa8623a160de8a718fc73167cf943570ce8
vlad11231 <67905804+vlad11231@users.noreply.github.com>
2026-03-04 19:18:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 # ==========================================
 # 1. CONFIGURARE
 # ==========================================
-TARGET_ADDRESS = "0x63ce342161250d705dc0b16df89036c8e5f9ba9a".lower()
+TARGET_ADDRESS = "0x8E9cD5eC7a26d602b63B4bC4C193fEbB83c8eD64".lower()
 
 # Datele tale de Telegram:
 TELEGRAM_TOKEN = "8261089656:AAF_JM39II4DpfiFzVTd0zsXZKtKcDE5G9A"
